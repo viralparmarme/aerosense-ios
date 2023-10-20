794680dc9abcd3ddb8a5ec3ccde23ec21880d30a
Viral Parmar <viral@Virals-MacBook-Air.local>
2023-10-20 11:03:02 +0530

    Add initial project filest

---
+++ AeroSense/AeroSense/AeroSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroSenseApp.swift
+//  AeroSense
+//
+//  Created by Viral Parmar on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AeroSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AeroSense/AeroSense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AeroSense
+//
+//  Created by Viral Parmar on 20/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
